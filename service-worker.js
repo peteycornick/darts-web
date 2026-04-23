576f28142974c8c4b09a5bd5606ee7ea9488d6fd
peteycornick <peteycornick@gmail.com>
2026-04-24 00:28:44 +0100

    pwa req

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,11 @@
-const CACHE_NAME = 'darts-v9';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+const CACHE_NAME = 'chat-v1';
 
-// Install Event
 self.addEventListener('install', (e) => {
-  self.skipWaiting(); // Force the waiting service worker to become the active service worker
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  self.skipWaiting();
 });
 
-// Activate Event to clear old caches
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    }).then(() => self.clients.claim()) // Take control of all open tabs immediately
-  );
-});
-
-// Fetch Event (Offline Capability)
 self.addEventListener('fetch', (e) => {
-  // Network-First Strategy
   e.respondWith(
-    fetch(e.request).then((response) => {
-      return caches.open(CACHE_NAME).then((cache) => {
-        cache.put(e.request, response.clone());
-        return response;
-      });
-    }).catch(() => caches.match(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
